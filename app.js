400dabc1b0a677120ddfbcb29035fad176429f10
Jordan S <77416389+Jordan-0618@users.noreply.github.com>
2021-05-22 14:21:35 -0700

    commit

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
 // Create function to read json
 
 function getMetaData(sample){ 
-    console.log("hello");  
+    // console.log("hello");  
     d3.json("data/samples.json").then((data) => {
         console.log(data);
-
         var metadata = data.metadata;
         console.log(metadata);
         
-        // filter data
+        // filter data for Object with selected number
 
-        var filterData = metadata.filter(sampleObject => sampleObject.id == sample);
-        var result = filterData[0];
-        // console.log(filterData);
-        console.log(fliterData);
+        // var dataArray = metadata.samples.filter(sampleObject => sampleObject.id.toString() === sample)[0];
+        // var dataArray = metadata.filter(metadata => metadata.id == sample);
+        // // const range = (start, end, length = end - start + 1) =>
+        // //     dataArray.from({length}, (_,i) => start + i);
+        // var result = dataArray[0];
+        // console.log(dataArray);
 
-        // console.log(result);
+        // // console.log(result);
 
-        var panel = d3.select("#sample-metadata");
+        // var panel = d3.select("#sample-metadata");
 
-        //Clear data in html
+        // //Clear data in html
 
-        panel.html("");
+        // panel.html("");
 
-        //add key and value to panel
+        // //add key and value to panel
         
-        // Object.entries(filterResult).forEach(([key, value]) => {
-        //     panel.append("h6").text(`${key}: ${value}`);
-        // console.log(d3.entries(fliterResult));
-
-
-        
-        // const obj; 
-        // const newObj = obj || {};
-        // const obj = null;
-        // const newObj = obj || {};
-        //     if(obj === null) {return null};
-        
-
+        // Object.entries(metadata).forEach((result) => {
+        //     panel.append("h6").text(result[0] + ":" + result[1]);
         // });
+        
     });
 }
 
@@ -46,18 +37,19 @@ getMetaData();
 
 function createChart(sample) {
     d3.json("data/samples.json").then((data) => {
-        var chartData = data.chartData;
+        var chartData = data.samples;
         console.log(chartData);
-        // console.log("hello");
+        console.log("hello");
         var resultData = chartData.filter(sampleObject => sampleObject.id == sample);
         
-        // var results = resultData[0];
+        var results = resultData[0];
+        console.log(results);
 
-        // console.log("hello");
+//         // console.log("hello");
 
-        // var otu_ids = results.otu_ids;
-        // var otu_labels = results.otu_labels;
-        // var sample_values = results.sample_values;
+//         // var otu_ids = results.otu_ids;
+//         // var otu_labels = results.otu_labels;
+//         // var sample_values = results.sample_values;
 
     });
 }
